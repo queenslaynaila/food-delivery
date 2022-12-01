bff33b90453d835356ae729c974323019f0bde55
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-12-01 15:38:19 +0300

    Configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// client/src/components/App.js
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+        <Routes>
+          <Route path="/testing" element={<h1>Test Route</h1>}/>
+          <Route path="/" element={<h1>Page Count: {count}</h1>}/>
+        </Routes>
+
+    </BrowserRouter>
   );
 }
 

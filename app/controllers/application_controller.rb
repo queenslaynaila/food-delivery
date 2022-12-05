85c4f935ff191a228ec8592901ff8d6ffd3f611c
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-12-05 10:33:08 +0300

    skip authorization for create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  # before_action :authorized
-  # def authorized
-  #   return render json:{error: "Not Authorized"},status: :unauthorized unless session.include? :customer_id
-  # end
+  before_action :authorized
+  def authorized
+    return render json:{error: "Not Authorized"},status: :unauthorized unless session.include? :customer_id
+  end
 end

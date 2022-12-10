4c8ed7b81b69ab2f9e2eecda99402def402f8a77
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-12-10 17:23:13 +0300

    renderbuildfile

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -6,9 +6,16 @@ import {useNavigate} from "react-router-dom"
 export default function Cart({order}) {
 
   let navigate = useNavigate()
+
    const [subtotal,SetSubTotal] = useState(0)
 
+      if (order.length > 0){
+        let totalPrice = order.reduce(function (accumulator, item) {
+         return accumulator + (item.price)
 
+        }, 0)
+        console.log(totalPrice)
+      }
 
 
      function removeItem(item){
